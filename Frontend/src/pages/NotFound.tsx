61c8bc0c7da1b6f056bc55078212141966a67e6d
Satish-devara <sd1761256@gmail.com>
2025-07-30 22:39:35 +0530

    useeffect function updated

--- Frontend/src/pages/NotFound.tsx
+++ Frontend/src/pages/NotFound.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 const NotFound = () => {
   const location = useLocation();
 
+  //using useeffect for making it affect after clicking on any error
   useEffect(() => {
     console.error(
       "404 Error: User attempted to access non-existent route:",
